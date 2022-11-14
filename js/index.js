35566b8550bf88542dc96b56a754474fc63589fe
Cintia <cin.m.pujol@gmail.com>
2022-11-14 18:59:24 -0300

    Mitad de proceso

--- js/index.js
+++ js/index.js
@@ -5,75 +5,43 @@
 let partido = ""
 let votosxpartido = ""
 
-function candidato (partido , votosxpartido) {
+function candidato(partido, votosxpartido) {
     partido = prompt('Ingrese el nombre de un partido: ')
-    votosxpartido = prompt('Ingrese el número de votos obtenidos: ')
-    console.log("El partido " + partido + " obtuvo " + votosxpartido + " votos ") 
+    votosxpartido = Number(prompt('Ingrese el número de votos obtenidos: '))
+    console.log("El partido " + partido + " obtuvo " + votosxpartido + " votos ")
+    return candidato
 }
 
-candidato (partido, votosxpartido)
-candidato (partido, votosxpartido)
-candidato (partido, votosxpartido)
-candidato (partido, votosxpartido)
-candidato (partido, votosxpartido)
+candidato()
+candidato()
+candidato()
+candidato()
+candidato()
 
 
-// Acá voy a exluir a los partidos que no hayan obtenido el 3% de los votos del total del padrón electoral
+// Acá voy a excluir a los partidos que no hayan obtenido el 3% de los votos del total del padrón electoral
 let padron = Number(prompt('Número de votantes según padrón electoral'))
 
-let porcentaje = Number((3*padron) / 100)
+let porcentaje = Number((3 * padron) / 100)
 console.log('El 3% del total es ' + porcentaje)
 
-
-function finalistas (votosxpartido, porcentaje) {
-    if (votosxpartido <= porcentaje) {
-        console.log('Va a tener posibilidades de obtener una banca')
-} else {
-    console.log('Queda excluido')
+let finalistas = function (votosxpartido, porcentaje) {
+    if (votosxpartido >= porcentaje) {
+        console.log(" y tiene posibilidad de obtener una Banca en el Congreso")
+    } else {
+        console.log(" queda eliminado del conteo")
+    }
 }
-}
 
-finalistas (votosxpartido, porcentaje)
-finalistas (votosxpartido, porcentaje)
-finalistas (votosxpartido, porcentaje)
-finalistas (votosxpartido, porcentaje)
-finalistas (votosxpartido, porcentaje)
-
-// Acá voy a obtener los dividendos
-
-let cargosvacantes = prompt('Número de bancas disponibles: ')
-
-for (let i = 1 ; i <= cargosvacantes ; i++) {
-       console.log('Resultado: ' + (votosxpartido / i ))
-} 
-
-// Asignación de cargos a los dividendos más altos
-
-
-
+finalistas()
+finalistas()
+finalistas()
+finalistas()
+finalistas()
+finalistas()
 
 
 /*
-let padron = ""
-
-function finalistas (votosxpartido, padron) {
-    padron = prompt('Número de votantes inscriptos en el padrón electoral: ')
-    return candidato
-    let porcentaje = (3*padron) / 100
-}
-/*
-
-
-
-
-if (porcentaje >= ((3*padron)/100)){
-    console.log('Va a tener posibilidad de obtener una banca')
-} else {
-    console.log('Está descartado')
-}
-/*
-
-
 // Acá se establecen los dividendos 
 
  let votos = Number(prompt('Votos válidamente emitidos: '))
@@ -83,4 +51,11 @@ if (porcentaje >= ((3*padron)/100)){
  for (let i = 1 ; i <= cargosvacantes ; i++) {
         console.log('Resultado: ' + (votos / i ))
  } 
- */
+ 
+
+// Asignación de cargos a los dividendos más altos
+
+if (finalistas >= cargosvacantes) {
+    console.log ("El partido va a obtener una Banca en el Congreso")
+}
+*/
