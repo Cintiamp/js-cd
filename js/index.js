56d45c95f9dc0c8bd990cef049dd530f45545a9e
Cintia <cin.m.pujol@gmail.com>
2023-01-19 20:16:15 -0300

    objetos y arrays

--- js/index.js
+++ js/index.js
@@ -2,18 +2,49 @@
 
 // Ingreso de datos de cantidad de votos por partido
 
+class Candidato{
+
+    constructor(nombre, votos){
+        this.nombre = nombre
+        this.votos = votos
+    }
+    candidatos() {
+        console.log('El partido ' + this.nombre + ' obtuvo ' + this.votos + ' votos ')
+    }
+
+}
 
 
-function candidato(partido, votosxpartido) {
+
+const candidato1 = new Candidato("FDT", "1200000")
+const candidato2 = new Candidato("JXC", "1300000")
+const candidato3 = new Candidato("FIT", "500000")
+const candidato4 = new Candidato("LIB", "800000")
+const candidato5 = new Candidato("MST", "200000")
+
+
+candidato1.candidatos()
+candidato2.candidatos()
+candidato3.candidatos()
+candidato4.candidatos()
+candidato5.candidatos()
+
+
+/*
+
+
+function candidato (partido, votosxpartido){
     for (let i = 1; i <= 5; i++) {
         partido = prompt('Ingrese el nombre de un partido: ')
         votosxpartido = Number(prompt('Ingrese el número de votos obtenidos: '))
         console.log("El partido " + partido + " obtuvo " + votosxpartido + " votos ")
     }
-    return candidato
+        
 }
 
-candidato()
+candidato ()
+
+*/
 
 
 // Acá voy a excluir a los partidos que no hayan obtenido el 3% de los votos del total del padrón electoral
@@ -23,20 +54,37 @@ let porcentaje = Number((3 * padron) / 100)
 console.log('El 3% del total es ' + porcentaje)
 
 
-function finalistas (votosxpartido, porcentaje) {
-    if (votosxpartido >= porcentaje) {
-        console.log(" y tiene posibilidad de obtener una Banca en el Congreso")
+let votos1 = candidato1.votos
+    if (candidato1.votos >= porcentaje) {
+        console.log(candidato1.nombre + " tiene posibilidad de obtener una Banca en el Congreso")
     } else {
-        console.log(" queda eliminado del conteo")
+        console.log(candidato1.nombre + " queda eliminado del conteo")
     }
-}
+let votos2 = candidato2.votos
+    if (candidato2.votos >= porcentaje) {
+        console.log(candidato2.nombre + " tiene posibilidad de obtener una Banca en el Congreso")
+    } else {
+        console.log(candidato2.nombre + " queda eliminado del conteo")
+    }
+let votos3 = candidato3.votos
+    if (candidato1.votos >= porcentaje) {
+        console.log(candidato3.nombre + " tiene posibilidad de obtener una Banca en el Congreso")
+    } else {
+        console.log(candidato3.nombre + " queda eliminado del conteo")
+    }
+let votos4 = candidato4.votos
+    if (candidato4.votos >= porcentaje) {
+        console.log(candidato4.nombre + " tiene posibilidad de obtener una Banca en el Congreso")
+    } else {
+        console.log(candidato4.nombre + "queda eliminado del conteo")
+    }
+let votos5 = candidato5.votos
+    if (candidato5.votos >= porcentaje) {
+        console.log(candidato5.nombre + " tiene posibilidad de obtener una Banca en el Congreso")
+    } else {
+        console.log(candidato5.nombre + " queda eliminado del conteo")
+    }
 
-finalistas()
-finalistas()
-finalistas()
-finalistas()
-finalistas()
-finalistas()
 
 
 // Acá se establecen los dividendos 
@@ -51,7 +99,28 @@ finalistas()
  
 
 // Asignación de cargos a los dividendos más altos
-
-if (finalistas >= cargosvacantes) {
-    console.log ("El partido va a obtener una Banca en el Congreso")
+const ganadores = []
+if (votos1 >= cargosvacantes) {
+    ganadores.push(candidato1.nombre)
+    console.log (candidato1.nombre + " va a obtener una Banca en el Congreso")
+}
+if (votos2 >= cargosvacantes) {
+    ganadores.push(candidato2.nombre)
+    console.log (candidato2.nombre + " va a obtener una Banca en el Congreso")
+}
+if (votos3 >= cargosvacantes) {
+    ganadores.push(candidato3.nombre)
+    console.log (candidato3.nombre + " va a obtener una Banca en el Congreso")
+}
+if (votos4 >= cargosvacantes) {
+    ganadores.push(candidato4.nombre)
+    console.log (candidato4.nombre + " va a obtener una Banca en el Congreso")
+}
+if (votos5 >= cargosvacantes) {
+    ganadores.push(candidato5.nombre)
+    console.log (candidato5.nombre + " va a obtener una Banca en el Congreso")
+}
+for (let index = 0; index < ganadores.length; index++){
+    const element = ganadores[index]
+    console.log(element)
 }
